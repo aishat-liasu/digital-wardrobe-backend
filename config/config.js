343b7b2a455846b0fdbda5910021868db5f48839
aishat-liasu <aishatliasu17965@gmail.com>
2025-11-13 20:25:59 +0100

    refactor: update code from ES5 to ES6 syntax

--- config/config.js
+++ config/config.js
@@ -1,28 +1,26 @@
-require("dotenv").config();
+import "dotenv/config";
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: "postgres",
-  },
+export const development = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+};
+export const test = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+};
+export const production = {
+  username: process.env.PROD_DB_USERNAME,
+  password: process.env.PROD_DB_PASSWORD,
+  database: process.env.PROD_DB_NAME,
+  host: process.env.PROD_DB_HOSTNAME,
+  port: process.env.PROD_DB_PORT,
+  dialect: "postgres",
 };
