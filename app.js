161d008abdedbb30753ce237d7c33edb35d04b92
aishat-liasu <aishatliasu17965@gmail.com>
2025-11-13 21:12:38 +0100

    feat: add auth and user routes to app server

--- app.js
+++ app.js
@@ -1,16 +1,70 @@
-const express = require("express");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { testConnection } from "./config/db.js";
+import errorMiddleware from "./middleware/errorHandler.middleware.js";
+import { logger } from "./utils/logger.js";
+import AuthRoutes from "./routes/auth.routes.js";
+import UserRoutes from "./routes/user.routes.js";
 
-const app = express();
-const port = 3000;
+export class App {
+  constructor(routes) {
+    this.app = express();
+    this.env = process.env.NODE_ENV || "development";
+    this.port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to my homepage</h1>");
-});
+    this.connectToDatabase();
+    this.initializeMiddlewares();
+    this.initializeRoutes(routes);
+    this.initializeErrorHandling();
+  }
 
-app.all("*", (req, res) => {
-  res.status(404).send("404 - Page not found");
-});
+  listen() {
+    this.app.listen(this.port, () => {
+      logger.info(`=================================`);
+      logger.info(`======= ENV: ${this.env} =======`);
+      logger.info(`App listening on the port ${this.port}`);
+      logger.info(`=================================`);
+    });
+  }
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+  getServer() {
+    return this.app;
+  }
+
+  async connectToDatabase() {
+    testConnection();
+  }
+
+  initializeMiddlewares() {
+    //this.app.use(morgan(LOG_FORMAT, { stream }));
+    //this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+
+    this.app.use(morgan(process.env.NODE_ENV));
+    this.app.use(cors());
+    this.app.use(helmet());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  initializeRoutes(routes) {
+    routes.forEach((route) => {
+      this.app.use("/", route.router);
+    });
+    this.app.get("/", (req, res) => {
+      res.status(200).json({ message: "Digital Wardrobe API is working" });
+    });
+    this.app.use((req, res) => {
+      res.status(404).json({ message: "Route not found" });
+    });
+  }
+
+  initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+}
+
+const app = new App([new AuthRoutes(), new UserRoutes()]);
+app.listen();
