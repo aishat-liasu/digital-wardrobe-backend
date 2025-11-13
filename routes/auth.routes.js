4fabc9a83db10028f992c4867aca0de303c5604b
aishat-liasu <aishatliasu17965@gmail.com>
2025-11-13 20:07:33 +0100

    feat: create routes for authentication

---
+++ routes/auth.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import AuthController from "../controllers/auth.controller.js";
+
+export default class AuthRoutes {
+  path = "/api/auth";
+  router = Router();
+  authController = new AuthController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    this.router.post(`${this.path}/signup`, this.authController.signUp);
+    this.router.post(`${this.path}/confirm`, this.authController.confirmSignUp);
+    this.router.post(`${this.path}/verify-otp`, this.authController.verifyOtp);
+    this.router.get(`${this.path}/login`, this.authController.login);
+    this.router.get(
+      `${this.path}/resend-code`,
+      this.authController.resendVerificationCode
+    );
+    this.router.get(`${this.path}/user`, this.authController.getUserData);
+  }
+}
