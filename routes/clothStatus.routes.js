597375580c6f3583fa9575f167e78e96700d6d51
aishat-liasu <aishatliasu17965@gmail.com>
2025-12-19 23:08:38 +0100

    feat: create routes for the CRUD operations on the cloth_statuses table

---
+++ routes/clothStatus.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import ClothStatusController from "../controllers/clothStatus.controller.js";
+import verifyCognitoToken from "../middleware/auth.middleware.js";
+
+export default class ClothStatusRoutes {
+  path = "/api/cloth-statuses";
+  router = Router();
+  controller = new ClothStatusController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    this.router.get(`${this.path}`, this.controller.getAll);
+    this.router.get(`${this.path}/:id`, this.controller.getById);
+
+    // this.router.post("/", verifyCognitoToken, this.controller.create);
+    // this.router.delete("/:id", verifyCognitoToken, this.controller.delete);
+  }
+}
