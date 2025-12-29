bbf41e722ceacf4f6dad60ddd0a29baa29f61faa
aishat-liasu <aishatliasu17965@gmail.com>
2025-12-29 23:56:37 +0100

    feat: remove the note column on the outfit_items model

--- models/outfit_item.model.js
+++ models/outfit_item.model.js
@@ -8,15 +8,15 @@ const OutfitItem = sequelize.define(
       type: DataTypes.UUID,
       allowNull: false,
       primaryKey: true,
+      references: { model: "outfits", key: "id" },
+      onDelete: "CASCADE",
     },
     clothId: {
       type: DataTypes.UUID,
       allowNull: false,
       primaryKey: true,
-    },
-    note: {
-      type: DataTypes.TEXT,
-      allowNull: true,
+      references: { model: "clothes", key: "id" },
+      onDelete: "CASCADE",
     },
   },
   {
