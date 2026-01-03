376c108d08b0b2446c4ec60de1ba4eff65a9de6c
aishat-liasu <aishatliasu17965@gmail.com>
2026-01-03 22:14:05 +0100

    feat: add function to run migrations and seeder files automatically and add more routes

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { testConnection } from "./config/db.js";
+
+import { testConnection, migrator, seeder } from "./config/db.js";
 import errorMiddleware from "./middleware/errorHandler.middleware.js";
 import { logger } from "./utils/logger.js";
+
+// Routes
 import AuthRoutes from "./routes/auth.routes.js";
 import UserRoutes from "./routes/user.routes.js";
+import StorageRoutes from "./routes/storage.routes.js";
+
+import ClothStatusRoutes from "./routes/clothStatus.routes.js";
+import ClothTypeRoutes from "./routes/clothType.routes.js";
+import ClothRoutes from "./routes/cloth.routes.js";
+
+import OutfitOccasionRoutes from "./routes/outfitOccasion.routes.js";
+import OutfitTagRoutes from "./routes/outfitTag.routes.js";
+import OutfitRoutes from "./routes/outfit.routes.js";
 
 export class App {
   constructor(routes) {
@@ -15,7 +27,6 @@ export class App {
     this.env = process.env.NODE_ENV || "development";
     this.port = process.env.PORT || 3000;
 
-    this.connectToDatabase();
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeErrorHandling();
@@ -30,12 +41,41 @@ export class App {
     });
   }
 
-  getServer() {
-    return this.app;
-  }
+  async initializeDatabase() {
+    await testConnection();
 
-  async connectToDatabase() {
-    testConnection();
+    // Run Migrations
+    if (process.env.RUN_MIGRATIONS === "true") {
+      logger.info("Checking for pending migrations...");
+
+      try {
+        const migrations = await migrator.up();
+        if (migrations.length > 0) {
+          logger.info(`Executed ${migrations.length} migrations`, migrations);
+        } else {
+          logger.info("Database is up to date (no new migrations).");
+        }
+      } catch (err) {
+        logger.error("Migration Failed:", err);
+        process.exit(1);
+      }
+    } else {
+      logger.info("Skipping Migrations...");
+    }
+
+    // Run Seeders
+    if (process.env.RUN_SEEDERS === "true") {
+      logger.info("Running seeders...");
+      try {
+        await seeder.up();
+        logger.info("Seeders executed successfully.");
+      } catch (err) {
+        logger.error("Seeding Failed:", err);
+        process.exit(1);
+      }
+    } else {
+      logger.info("Skipping Seeders...");
+    }
   }
 
   initializeMiddlewares() {
@@ -43,7 +83,7 @@ export class App {
     //this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
 
     this.app.use(morgan(process.env.NODE_ENV));
-    this.app.use(cors());
+    this.app.use(cors({ origin: "http://localhost:5173", credentials: true }));
     this.app.use(helmet());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
@@ -51,7 +91,7 @@ export class App {
 
   initializeRoutes(routes) {
     routes.forEach((route) => {
-      this.app.use("/", route.router);
+      this.app.use(route.path, route.router);
     });
     this.app.get("/", (req, res) => {
       res.status(200).json({ message: "Digital Wardrobe API is working" });
@@ -66,5 +106,26 @@ export class App {
   }
 }
 
-const app = new App([new AuthRoutes(), new UserRoutes()]);
-app.listen();
+const startServer = async () => {
+  const app = new App([
+    new AuthRoutes(),
+    new UserRoutes(),
+    new ClothTypeRoutes(),
+    new ClothStatusRoutes(),
+    new StorageRoutes(),
+    new ClothRoutes(),
+    new OutfitOccasionRoutes(),
+    new OutfitTagRoutes(),
+    new OutfitRoutes(),
+  ]);
+
+  try {
+    await app.initializeDatabase();
+    app.listen();
+  } catch (err) {
+    logger.error("Critical Failure during startup:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
