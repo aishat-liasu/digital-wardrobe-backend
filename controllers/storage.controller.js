c48da07e1b3d72375a59872e78292f9689588e49
Aishat <aishatliasu17965@gmail.com>
2026-06-26 14:07:38 +0100

    feat: add business logic for storage

---
+++ controllers/storage.controller.js
@@ -0,0 +1,28 @@
+import StorageService from "../services/storage.service.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+class StorageController {
+  storageService = new StorageService();
+
+  getPresignedUrl = catchAsync(async (req, res, next) => {
+    const { fileName, fileType, folder } = req.body;
+
+    const cognitoId = req.user.cognitoId;
+    const allowedFolders = ["clothes", "outfits", "avatars"];
+    const targetFolder = allowedFolders.includes(folder) ? folder : "misc";
+
+    const data = this.storageService.generatePresignedUrl({
+      fileName,
+      fileType,
+      targetFolder,
+      cognitoId,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data,
+    });
+  });
+}
+
+export default StorageController;
