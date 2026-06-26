01c1555d3094f9c0851248c9c3f9b8cd79fbae46
Aishat <aishatliasu17965@gmail.com>
2026-06-26 17:00:01 +0100

    chore: remove redundant code

--- config/aws.js
+++ config/aws.js
@@ -1,32 +1,12 @@
-const AWS = require("@aws-sdk/client-s3");
+import { S3Client } from "@aws-sdk/client-s3";
+import { config } from "./index.js";
 
-// AWS region
-const REGION = process.env.AWS_REGION || "us-east-1";
-
-// Initialize AWS SDK
-AWS.config.update({
-  region: REGION,
+const s3Client = new S3Client({
+  region: config.aws.region,
+  credentials: {
+    accessKeyId: config.aws.s3AccessKey,
+    secretAccessKey: config.aws.s3SecretKey,
+  },
 });
 
-// S3 bucket for cloth images
-const s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
-});
-
-// Cognito for authentication
-const cognito = new AWS.CognitoIdentityServiceProvider({
-  apiVersion: "2016-04-18",
-});
-
-// Secrets Manager for environment variables and credentials
-const secretsManager = new AWS.SecretsManager({
-  apiVersion: "2017-10-17",
-});
-
-module.exports = {
-  AWS,
-  REGION,
-  s3,
-  cognito,
-  secretsManager,
-};
+export { s3Client };
