54278134f2f376d4226cd81de72baeb392879af6
Aishat <aishatliasu17965@gmail.com>
2026-06-26 13:18:33 +0100

    feat: add data validation to cloth routes

--- routes/cloth.routes.js
+++ routes/cloth.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import ClothController from "../controllers/cloth.controller.js";
 import verifyCognitoToken from "../middleware/auth.middleware.js";
+import { validateRequest } from "../middleware/validation.middleware.js";
+import { createClothSchema, updateClothSchema } from "../validations/cloth.validation.js";
+import { paginationQuerySchema } from "../validations/query.validation.js";
 
 export default class ClothRoutes {
   path = "/api/clothes";
@@ -13,11 +16,26 @@ export default class ClothRoutes {
 
   initializeRoutes() {
     this.router.use(verifyCognitoToken);
-    this.router.get("/stats", this.clothController.getClothStats);
-    this.router.get("/", this.clothController.getAllClothes);
-    this.router.post("", this.clothController.createCloth);
+    this.router.get(
+      "/", 
+      validateRequest(paginationQuerySchema, "query"), 
+      this.clothController.getAllClothes
+    );
+
+    this.router.post(
+      "/",
+      validateRequest(createClothSchema),
+      this.clothController.createCloth
+    );
+
     this.router.get("/:id", this.clothController.getClothById);
-    this.router.put("/:id", this.clothController.updateCloth);
+
+    this.router.put(
+      "/:id",
+      validateRequest(updateClothSchema),
+      this.clothController.updateCloth
+    );
+
     this.router.delete("/:id", this.clothController.deleteCloth);
   }
 }
